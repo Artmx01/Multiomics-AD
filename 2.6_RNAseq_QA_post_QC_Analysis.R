f74eb01dc9218231ad20768301e6bca07e07d01c
Artmx01 <101605814+Artmx01@users.noreply.github.com>
2026-09-23 14:33:40 -0600

    Update and rename RNAseq_QA_post_QC_Analysis.R to 2.6_RNAseq_QA_post_QC_Analysis.R
    Workflow is added

--- 2.6_RNAseq_QA_post_QC_Analysis.R
+++ 2.6_RNAseq_QA_post_QC_Analysis.R
@@ -2,6 +2,7 @@
 # QA Analysis (postQC): data filtered
 #####################################
 
+
 # Packages
 library(vroom)     # ‘1.7.1’
 library(tidyverse) # ‘2.0.0’
@@ -10,17 +11,21 @@ library(biomaRt)   # ‘2.68.0’
 library(EDASeq)    # ‘2.46.0’
 
 
-# QA analysis workflow:
+
+#      -- Workflow -- 
+#
 # 1. Check low counts
 # 2. Check GC bias
 # 3. Check Length bias
 # 4. Check RNA compostion bias
 # 5. Check meanVar bias
 # 6. PCA
+#
 # Save filtered data
 
 
-# 1. Check low counts:
+
+# 1. ------------------------- Check low counts -------------------------
 
 mycountsbio_filtered <- dat(noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch,
                    type =  "countsbio",
@@ -42,7 +47,8 @@ explo.plot(mycountsbio_filtered,
 dev.off()
 
 
-# 2. Check GC bias:
+
+# 2. ------------------------- Check GC bias -------------------------
 
  myGCcontent_filtered <- dat(noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch,
                    k = 0,            # A feature is considered to be detected if the corresponding number of read counts is > k.
@@ -69,7 +75,8 @@ explo.plot(myGCcontent_AD_filtered,
 dev.off()
 
 
-# 3. Check Length bias:
+
+# 3. ------------------------- Check Length bias -------------------------
 
  mylengthbias_filtered <- dat(noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch,
                     k = 0,
@@ -96,7 +103,8 @@ explo.plot(mylengthbias_AD_filtered,
 dev.off()
 
 
-# 4. Check RNA compostion bias:
+
+# 4. ------------------------- Check RNA compostion bias -------------------------
 
 rna_comp_bias_filtered <- dat(input = noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch, type = "cd", norm = TRUE)
 
@@ -105,7 +113,8 @@ explo.plot(rna_comp_bias_filtered, samples = 1:12)
 dev.off()
 
 
-# 5. Check meanVar bias:
+
+# 5. ------------------------- Check meanVar bias -------------------------
 
 # Average gene expression
 avg_gene_exps_filtered_215 <- rowMeans(exprs(noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch))
@@ -118,17 +127,27 @@ pdf("meanVar_filtered_215.pdf")
 plot(x = avg_gene_exps_filtered_215, y = gene_vars_filtered_215, log = "xy", main = "Mean-Variance relation", xlab = "Mean expression (log scale)", ylab = "Variance (log scale)")
 dev.off()
 
+# Scaling is needed (performed in sgcca function (wrapper.sgcca))
 
-# 6. PCA:
 
-pca_rnaseq_filtered_215 <- prcomp(x = t(exprs(noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch)), center = TRUE, scale. = FALSE)
 
-pca_rnaseq_filtered_215_df <- data.frame(
-  sample = rownames(pca_rnaseq_filtered_215$x),
-  X = pca_rnaseq_filtered_215$x[,1],
-  Y = pca_rnaseq_filtered_215$x[,2]
+# 6. ------------------------- PCA -------------------------
+
+# Get PCA
+pca_rnaseq_filtered_215 <- prcomp(
+  x      = t(exprs(noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch)), 
+  center = TRUE,
+  scale. = FALSE
 )
 
+# PCA dataframe (for plotting)
+pca_rnaseq_filtered_215_df <- data.frame(
+  sample = rownames(pca_rnaseq_filtered_215$x),
+  X      = pca_rnaseq_filtered_215$x[,1],
+  Y      = pca_rnaseq_filtered_215$x[,2]
+)
+
+# Get % variance per PC
 pca_var_rnaseq_filtered_215 <- pca_rnaseq_filtered_215$sdev^2
 
 pca_var_rnaseq_filtered_215_per <- round(pca_var_rnaseq_filtered_215 / sum(pca_var_rnaseq_filtered_215) * 100 , 1)
@@ -136,6 +155,7 @@ pca_var_rnaseq_filtered_215_per <- round(pca_var_rnaseq_filtered_215 / sum(pca_v
 identical(pca_rnaseq_filtered_215_df$sample, RNA_seq_metadata_filteredQC_DLPFC_215$specimenID)
 # [1] TRUE
 
+# Plot PCA by ADcontrol
 pdf("pca_rnaseq_filtered_215_isAD.pdf")
   pca_rnaseq_filtered_215_df %>%
  ggplot(mapping = aes(x = X, y = Y)
@@ -165,8 +185,8 @@ pdf("pca_rnaseq_filtered_215_batch.pdf")
       stat_ellipse(geom = "polygon", aes(fill = as.factor(RNA_seq_metadata_filteredQC_DLPFC_215$sequencingBatch)), alpha = 0.2, show.legend = FALSE)
 dev.off()
 
-######################################
-######################################
+
+# ------------------------- QA Analysis finished (post QC) -------------------------
 
 # Save filtered data
 saveRDS(object = exprs(noiseqData_filtered_215_CPM_noGCbias_TMM_selectedFeatures_noBatch), file = "ROSMAP_RNAseq_counts_filtered_215.rds")
